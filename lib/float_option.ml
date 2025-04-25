4b1ebec5d9d474603a75514f78f59e8a18b32b08
Stefan Muenzel <source@s.muenzel.net>
2025-04-25 16:31:19 +0200

    WIP

---
+++ lib/float_option.ml
@@ -0,0 +1,65 @@
+open Sexplib0
+open Sexplib0.Sexp_conv
+
+type t = float
+
+let none = Float.nan
+
+let some x =
+  assert (not (Float.is_nan x));
+  x
+
+let of_option = function
+  | None -> none
+  | Some x -> some x
+
+let ( + ) = ( +. )
+let ( - ) = ( -. )
+
+module Optional_syntax = struct
+  module Optional_syntax = struct
+    let is_none = Float.is_nan
+    let unsafe_value t = t
+  end
+end
+
+open Optional_syntax
+
+let value ~default t =
+  match%optional t with
+  | None -> default
+  | Some x -> x
+
+let sexp_of_t (t : t) =
+  match%optional t with
+  | None -> Sexp.Atom "None"
+  | Some x -> [%sexp_of: float] x
+
+let t_of_sexp (s : Sexp.t) =
+  match s with
+  | Atom "None" -> none
+  | _ -> some ([%of_sexp: float] s)
+
+module Array = struct
+  module A = Stdlib.Float.Array
+  type t = floatarray
+
+  let sexp_of_t (t : t) =
+    A.map_to_array sexp_of_t t
+    |> [%sexp_of: Sexp.t array]
+
+  let t_of_sexp (s : Sexp.t) =
+    [%of_sexp: Sexp.t array] s
+    |> A.map_from_array t_of_sexp
+
+  let of_option_list (l : float option list) =
+    List.map of_option l
+    |> A.of_list
+
+  let of_option_array_map (a : 'a option array) ~f =
+    A.map_from_array
+      (function
+        | None -> none
+        | Some x -> some (f x))
+      a
+end
---
+++ lib/float_option.mli
@@ -0,0 +1,27 @@
+
+type t = private float
+
+val of_option : float option -> t
+
+val none : t
+val some : float -> t
+
+val value : default:float -> t -> float
+
+val ( + ) : t -> t -> t
+val ( - ) : t -> t -> t
+
+module Optional_syntax : sig
+  module Optional_syntax : sig
+    type value := float
+    val is_none : t -> bool
+    val unsafe_value : t -> value
+  end
+end
+
+module Array : sig
+  type t = private Float.Array.t [@@deriving sexp]
+
+  val of_option_list : float option list -> t
+  val of_option_array_map : 'a option array -> f:('a -> float) -> t
+end
